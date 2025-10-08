ac123660a30be96704a6babb34e7cb9e77e671d8
yui-amai <73156864+yui-amai@users.noreply.github.com>
2025-10-08 23:54:10 +0900

    upload ADT 20251007

--- ADT/abc336_c.R
+++ ADT/abc336_c.R
@@ -2,4 +2,20 @@
 # See problem: https://atcoder.jp/contests/abc336/tasks/abc336_c
 # 2025-10-07 AC+ (ADT　20251007　EASY)
 
-N <- scan("stdin", what = integer(), quiet = TRUE)
+options(scipen = 100, digits = 15)
+N <- scan("stdin", what = double(), quiet = TRUE)
+
+x <- N - 1
+
+if (x == 0) {
+  cat("0\n")
+} else {
+  digs <- double(0)
+  while (x > 0) {
+    d <- x %% 5
+    digs <- c(digs, as.integer(2 * d))
+    x <- x %/% 5
+  }
+  ans <- paste(rev(digs), collapse = "")
+  cat(ans, "\n", sep = "")
+}
