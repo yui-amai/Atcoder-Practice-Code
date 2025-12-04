66928ec6ea103fd7c37ee5a7b78ee06af2726cac
yui-amai <73156864+yui-amai@users.noreply.github.com>
2025-12-04 20:49:33 +0900

    upload ADT 20251204

---
+++ ADT/abc238_a.R
@@ -0,0 +1,7 @@
+# ABC238 A - Exponential or Quadratic
+# See problem: https://atcoder.jp/contests/abc238/tasks/abc238_a
+# 2025-12-04 AC+ (ADT　20251204　EASY)
+
+n <- scan("stdin", what = double(), quiet = TRUE)
+
+if (n >= 2 && n <= 4) cat("No\n") else cat("Yes\n")
